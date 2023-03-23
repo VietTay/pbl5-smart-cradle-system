f63d1decb8843e9a883fd9ad415a4c615066a237
Vuong Viet Tay <tayvuong46@gmail.com>
2023-03-23 11:13:22 +0700

    login and signup

---
+++ smart-cradle-system/SmartCradleApplication/app/src/main/java/com/example/smartcradleapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.smartcradleapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView userName;
+    Button logout;
+    FirebaseAuth auth;
+    FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        logout = findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+            }
+        });
+    }
+
+
+}
